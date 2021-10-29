8e0e80cea5f50a5b2b46593997b63dfcc1273d87
Giovanni Reniero <giovanni.reniero@gmail.com>
2021-10-29 18:06:01 +0100

    Added JS for dropdown menu

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+document.addEventListener("click", e =>{
+
+  const isDropdownlink = e.target.matches("[data-dropdown-link]")
+
+  if (!isDropdownlink && e.target.closest("[data-dropdown]") !=null) return
+  
+  let currentDropdown 
+  if (isDropdownlink ) {  
+    currentDropdown = e.target.closest("[data-dropdown]")
+    console.log("Go")
+    currentDropdown.classList.toggle("active")
+  } 
+
+  document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+    if (dropdown === currentDropdown) return;
+    dropdown.classList.remove("active")
+    console.log("Stop")
+
+   })
+})
+
