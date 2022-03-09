82042955a266e127041c008c075f35f56c8aef03
Giovanni Reniero <giovanni.reniero@gmail.com>
2022-03-09 20:30:55 +0000

    Fixed issue with travel dropdown menu

--- js/script.js
+++ js/script.js
@@ -21,3 +21,22 @@ document.addEventListener("click", e =>{
 
 
 
+document.addEventListener("click", e =>{
+  const isDropdownlinkTravel = e.target.matches("[data-dropdown-link-travel]")
+  // console.log(e)
+  if (!isDropdownlinkTravel && e.target.closest("[data-dropdown-travel]") !=null)
+   return
+  // console.log("Hello")
+  
+  let currentDropdownTravel 
+  if (isDropdownlinkTravel ) {  
+    currentDropdownTravel = e.target.closest("[data-dropdown-travel]")
+    currentDropdownTravel.classList.toggle("active")
+  } 
+
+  document.querySelectorAll("[data-dropdown-travel].active").forEach(dropdownTravel => {
+    if (dropdownTravel === currentDropdownTravel) return;
+    dropdownTravel.classList.remove("active")
+    console.log("Stop")
+   })
+})
